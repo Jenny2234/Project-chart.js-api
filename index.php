b904a7f43ab63677916751ba84a867e08e9c29df
Jenny2234 <98068500+Jenny2234@users.noreply.github.com>
2022-03-14 14:35:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,254 +1,101 @@
-<!DOCTYPE html>
+<?php
+require_once("db.php");
+?>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
     <title>各分位載具消費張數金額</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<style>
-    .bg{
-        /* 800X559 */
-        height: 680px;
-        width: 480px;
-        background: url("台灣地圖.jpg");
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: center;
-    }
-</style>
-</head>
-<body>
-    
-    <div class="text-center"><h2>各縣市 <a style="color: brown;">電腦系統設計服務業</a> <small>消費金額總平均</small></h2></div>
-    <div class="container bg">
-        <p>&emsp; </p><small class="fst-italic text-end">單位:萬</small>
-        <canvas id="myChart"></canvas>
-    </div>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        var ctx = document.getElementById("myChart");
-        const data = {
-            datasets: [
-            {
-    label: '   ',
-    data:[ {
-                    x: 0,
-                    y: 100,
-                    r: 1
-                }],
-    backgroundColor: 'rgb(60, 99, 132,0)',
-    },
-    {
-    label: '    ',
-    data:[ {
-                    x: 100,
-                    y: 100,
-                    r: 1
-                }],
-    backgroundColor: 'rgb(60, 99, 132,0)',
-    },
-    {
-    label: '    ',
-    data:[ {
-                    x: 100,
-                    y: 100,
-                    r: 1
-                }],
-    backgroundColor: 'rgb(60, 99, 132,0)',
-    },
-    {
-    label: '    ',
-    data:[ {
-                    x: 100,
-                    y: 100,
-                    r: 1
-                }],
-    backgroundColor: 'rgb(60, 99, 132,0)',
-    },
-    {
-    label: '    ',
-    data:[ {
-                    x: 0,
-                    y:0,
-                    r: 1
-                }],
-    backgroundColor: 'rgb(60, 99, 132,0)',
-    },
-    {
-    label: '    ',
-    data:[ {
-                    x: 0,
-                    y:0,
-                    r: 1
-                }],
-    backgroundColor: 'rgb(60, 99, 132,0)',
+  </head>
+  <style>
+    .text{
+    background: linear-gradient(to left, #3a1c71, #d76d77);
+    background: -webkit-linear-gradient(to top, #3a1c71, #d76d77, #ffaf7b);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    font-size: 40px
     }
-    ,{
-      label: '台北市18.5165萬',
-      data:[{
-                	x: 72,
-                	y: 94,
-                    //r=半徑
-                	r: 0.6
-            	}],
-    backgroundColor: 'rgb(255, 99, 132)',
-    },
-    {
-      label: '新北市',
-      data:[ {
-                	x: 54,
-                	y: 98,
-                	r: 10
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '基隆市',
-      data:[ {
-                	x: 90,
-                	y:90,
-                	r: 10
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '桃園市',
-      data:[ {
-                	x: 90,
-                	y: 45,
-                	r: 10
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '宜蘭市',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '嘉義市',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '新竹縣',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '新竹市',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '台南市',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '台中市',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '彰化縣',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '雲林縣',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '高雄市',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '嘉義縣',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    },
-    {
-      label: '花蓮縣',
-      data:[ {
-                	x: 0,
-                	y: 100,
-                	r: 1
-            	}],
-    backgroundColor: 'rgb(60, 99, 132)',
-    }
-  ]
- };
+  </style>
+  <body>
+    <!-- php -->
+    <?php
+        $sql="SELECT*FROM march11 ORDER BY id ASC;";
+        $result= $conn -> query($sql);
+        $data=$result->fetch_all(MYSQLI_ASSOC);
+        $datapolar= json_encode($data);
+    ?>
+        <!-- php end -->
+    <div class="text-center"><h1>各分位載具消費張數金額</h1></div>
+    <div class="text-center">
+        <h3>各縣市 <a class="text">電腦系統設計服務業</a> 支付載具_消費金額總平均</h4>
+    </div>
+    <div class="container" style="position:relative;height:40vh;width:40vw">
+    <canvas id="myChart"></canvas>
+    <small class="fst-italic text-end">單位:百</small>
+    </div>
 
- const config = {
-        	type: 'bubble',
-        	data: data,
-        	options: {
-            		maintainAspectRatio: false,
-                    plugins: {
-                        legend:{
-                            position: 'bottom',
-                            labels:{
-                                padding:10
-            }
-    // here goes original or customized code of your generateLabels callback
-}
-                            }
-                        
-                    
-        	},
-            
-};
-
-
-
-        const myChart = new Chart(ctx,config)
-    </script>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-</body>
+    <!-- 第一張圖表jenny -->
+    <script>
+        let label1=[];
+        let data1=[];
+        let datapol=<?=$datapolar?>.map(function(item){
+            let datalet={
+                one:item["one"],
+                two:item["two"]
+            };
+        label1.push(datalet.one);
+        data1.push(datalet.two);
+        return datalet;
+        })
+        var ctx = document.getElementById('myChart');
+        const dataA = {
+        labels:label1,
+        datasets: [{
+            data:data1,
+            backgroundColor:[      
+                "rgb(207,186,240)",// 台北市
+                'rgb(163,196,243)',// 新北市
+                'rgb(144,219,244)',// 台中
+                'rgb(142,236,245)',// 基隆
+                'rgb(152,245,225)',// 台南
+                'rgb(185,251,192)',// 高雄
+                'rgba(251,248,204,5)',// 宜蘭
+                'rgb(253,228,207)',// 桃園
+                'rgba(208, 140, 96,0.6)',// 嘉義市
+                'rgba(122, 28, 11,0.4)',// 新竹縣
+                'rgba(173, 46, 36,0.5)',// 彰化縣
+                'rgb(222, 116, 135)',// 新竹市
+                'rgb(255,207,210)',// 雲林縣
+                'rgb(241,192,232)',// 嘉義縣
+                ],
+                hoverOffset: 20
+            }]
+        };
+
+        const config1 = {
+            type: 'doughnut',
+            data: dataA,
+            options: {
+                // responsive: true,
+                // maintainAspectRatio: false
+                aspectRatio: 2,
+                plugins:{
+                    legend:{
+                        position:'right'
+                    }
+                }
+            }
+        };
+        const myChart = new Chart(ctx, config1)
+    </script>
+  </body>
 </html>
